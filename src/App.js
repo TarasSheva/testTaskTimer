5c16cc02cab2bedaf9b9ded137dc73410bf3f66e
TarasSheva <tarasgsheva@gmail.com>
2021-01-07 13:23:41 +0200

    mod

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import './App.css';
+import Header from "./components/Header";
 
 function App() {
 
   return (
-    <div className="App">
+    <div className="container">
+      <Header />
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div className='header'>Таймер</div> 
+    )
+}
+export default Header;
